2c3a9101cd59897bfafcc813fe1acce478dbf9b9
Sheryl Lan <sheryllan@sheryldeair.home>
2018-03-05 00:58:32 +0000

    clean up the mehtods and make them static

--- webscraper/configparser.py
+++ webscraper/configparser.py
@@ -4,83 +4,68 @@ import openpyxl
 import os
 
 
-class InputParams(object):
+class XmlInputParams(object):
     def __init__(self, path, tags, attrs):
         self.path = path
         self.tags = tags
         self.attrs = attrs
 
 
-class OutputParams(object):
-    def __init__(self, path, sheets):
+class XlsxOutputParams(object):
+    def __init__(self, path, sheet2data):
         self.path = path
-        self.sheets = sheets
+        self.sheet2data = sheet2data
 
 
-class XlsxParser(object):
-
-
-    @classmethod
+class XmlParser(object):
+    @staticmethod
     # returns a list of dictionaries
-    def fltr_attrs(cls, tags, attrs=None):
+    def fltr_attrs(tags, attrs=None):
         if attrs is None:
             return tags
         return [{k: v for k, v in tag.attrs.items() if k in attrs} for tag in tags]
 
-    @classmethod
-    def parse(cls, path, tags=None):
+    @staticmethod
+    def parse(path, tags=None):
         with open(path, 'r') as input:
             raw_xml = input.read()
         soup = BeautifulSoup(raw_xml, 'html.parser')
         return soup if tags is None else soup.find_all(tags)
 
-    # @classmethod
-    # def dict_to_df(cls, dict, columns=None):
-    #     return pd.DataFrame(dict, columns=columns)
 
-    @classmethod
-    def create_xlsx(cls, filepath):
-        if not os.path.isfile(filepath):
-            wb = openpyxl.Workbook()
-            wb.save(filepath)
-        else:
-            wb = openpyxl.load_workbook(filepath)
-        return wb
+class XlsxWriter(object):
+    
+    @staticmethod
+    def load_xlsx(filepath):
+        # if not os.path.isfile(filepath):
+        #     wb = openpyxl.Workbook()
+        #     wb.save(filepath)
+        # else:
+        #     wb = openpyxl.load_workbook(filepath)
+        # return wb
+        return openpyxl.load_workbook(filepath) if os.path.isfile(filepath) else openpyxl.Workbook()
 
-    # @classmethod
-    # def df_to_xlsx(cls, df, outputs, override=True):
-    #     dest = outputs.path
-    #     wb = cls.create_xlsx(dest)
-    #     wrt = pd.ExcelWriter(dest, engine='openpyxl')
-    #     if not override:
-    #         wrt.book = wb
-    #         wrt.sheets = dict((ws.title, ws) for ws in wb.worksheets)
-    #     df.to_excel(wrt, outputs.sheet, index=False)
-    #     wrt.save()
-
-    @classmethod
-    def __config_xlwriter(cls, wrt, wb):
+    @staticmethod
+    def __config_xlwriter(wrt, wb):
         wrt.book = wb
         wrt.sheets = dict((ws.title, ws) for ws in wb.worksheets)
         return wrt
 
-    @classmethod
-    def df_to_xlsx(cls, df, wrt, sheet, override=True):
+    @staticmethod
+    def dict_to_xlsheet(data, wrt, sheet, columns=None, override=True):
+        df = pd.DataFrame(data) if columns is None else pd.DataFrame(data, columns)
         if not override:
-            cls.__config_xlwriter()
+            XlsxWriter.__config_xlwriter()
         df.to_excel(wrt, sheet, index=False)
 
-    @classmethod
-    def dict_to_xlsx(cls, dict, headers, wrt, sheet, override=True):
-        df = pd.DataFrame(dict, headers)
-        cls.df_to_xlsx(df, wrt, sheet, override)
-
-    @classmethod
-    def parse_to_save_sheets(cls, inParams, outParams):
-        for ip in inParams:
-            parsed = cls.fltr_attrs(cls.parse(inputs.path, inputs.tags), inputs.attrs)
+    @staticmethod
+    def parse_save_sheets(inParams, outParam, override=True):
+        wrt = pd.ExcelWriter(outParam.path, engine='openpyxl')
+        for in_param in zip(inParams, outParam.sheets):
+            data_parsed = XmlParser.fltr_attrs(XmlParser.parse(in_param.path, in_param.tags), in_param.attrs)
+            XlsxWriter.dict_to_xlsheet(data_parsed, wrt, )
             wrt = pd.ExcelWriter(outputs.path, engine='openpyxl')
-
+        wrt.save()
 
 
 base_path = '/home/slan/Documents/config_files/'
@@ -101,11 +86,10 @@ param_keys = ['tags', 'fields', 'sheet']
 tags = 'product'
 sheet = 'Config'
 
-a = 'fdfs'
-for aa in a:
-    print(isinstance([1,2,3], list))
+
 # for exch in exchanges:
 #     src = base_path + source_files[exch]
 #     dest = base_path + dest_files[exch]
 #     params = {param_keys[0]: tags, param_keys[1]: properties[exch], param_keys[2]: sheet}
 #     XlsxParser.parse_to_save(src, dest, params)
+XmlParser.fltr_attrs()
